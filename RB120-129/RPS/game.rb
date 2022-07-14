77232c4cb4be587d5b14c5e3e0c1e81318de5b52
Andrew Ho <andrew.gjh@gmail.com>
2022-07-14 12:53:05 -0400

    Add lizard spock to game play

--- RB120-129/RPS/game.rb
+++ RB120-129/RPS/game.rb
@@ -1,9 +1,37 @@
 class RPSGame
   attr_reader :human, :computer
-
+  attr_accessor :score
   def initialize
     @human = Human.new
     @computer = Computer.new
+    @score = {human: 0, computer: 0}
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      human.choose
+      computer.choose
+      winner = who_won?
+      tally(winner)
+      display_moves
+      display_win(winner)
+      display_score
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+
+  private
+
+  def tally(winner)
+    score[winner] += 1 unless winner == nil 
+  end
+
+  def display_score
+    puts "-" * 50
+    puts "#{human.name}: #{score[:human]}      #{computer.name}: #{score[:computer]}".center(50)
+    puts "-" * 50
   end
 
   def play_again?
@@ -17,30 +45,24 @@ class RPSGame
     answer == "y"
   end
 
-  def play
-    display_welcome_message
-    loop do
-      human.choose
-      computer.choose
-      display_moves
-      display_winner
-      break unless play_again?
-    end
-    display_goodbye_message
-  end
-
   def display_moves
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
   end
 
-  def display_winner
+  def who_won?
     if human.move > computer.move
-      puts "#{human.name} won!"
+      :human
     elsif human.move < computer.move
-      puts "#{computer.name} won!"
-    else
-      puts "It's a tie!"
+      :computer
+    end
+  end
+
+  def display_win(winner)
+    case winner
+    when :human then puts "#{human.name} won!"
+    when :computer then puts "#{computer.name} won!"
+    else puts "It's a tie!"
     end
   end
 
@@ -52,6 +74,7 @@ class RPSGame
   def display_goodbye_message
     puts "Thanks for playing Rock, Paper, Scissors. Goodbye!"
   end
+
 end
 
 class Player
@@ -77,12 +100,20 @@ class Human < Player
   def choose
     choice = nil
     loop do
-      puts "Please choose rock, paper or scissors"
-      choice = gets.chomp
-      break if Move::VALUES.include? choice
-      puts "Sorry, invalid choice"
+      puts <<-MOVE
+      \n
+      Please enter the number of your choice 
+        1)Rock 
+        2)Paper
+        3)Scissors
+        4)Lizard
+        5)Spock"
+      MOVE
+      choice = gets.chomp.to_i
+      break if (1..5).to_a.include? choice
+      puts "Sorry, invalid choice. Enter number between 1 - 5"
     end
-    self.move = Move.new(choice)
+    self.move = Move.new(Move::VALUES[choice - 1])
   end
 end
 
@@ -98,7 +129,7 @@ end
 
 class Move
   attr_reader :value
-  VALUES = ['rock', 'paper', 'scissors']
+  VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
   def initialize(value)
     @value = value
   end
@@ -115,17 +146,31 @@ class Move
     value == "paper"
   end
 
-  def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?)
+  def lizard?
+    value == "lizard"
+  end
+
+  def spock?
+    value == "spock"
+  end
+
+   def >(other_move)
+    (rock? && (other_move.scissors? || other_move.lizard?)) ||
+    (paper? && (other_move.rock? || other_move.spock?)) ||
+    (scissors? && (other_move.paper? || other_move.lizard?)) ||
+    (lizard? && (other_move.paper? || other_move.spock?)) ||
+    (spock? && (other_move.rock? || other_move.scissors?))
   end
 
   def <(other_move)
-    (rock? && other_move.paper?) ||
-      (paper? && other_move.scissors?) ||
-      (scissors? && other_move.rock?)
+    (rock? && (other_move.spock? || other_move.paper?)) ||
+    (paper? && (other_move.scissors? || other_move.lizard?)) ||
+    (scissors? && (other_move.spock? || other_move.rock?)) ||
+    (lizard? && (other_move.scissors? || other_move.rock?)) ||
+    (spock? && (other_move.paper? || other_move.lizard?))
   end
+
+ 
 
   def to_s
     value
